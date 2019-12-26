8c266695819e00ae453f8677ffc0561e390c1824
Andres obando <cometaue1@gmail.com>
2019-12-26 18:29:57 -0500

    Primera modificación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./login/register.component";
+import { PagesComponent } from "./pages/pages.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { Graficas1Component } from "./pages/graficas1/graficas1.component";
+import { ProgressComponent } from "./pages/progress/progress.component";
+import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { APP_ROUTES } from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagesComponent,
+    DashboardComponent,
+    Graficas1Component,
+    ProgressComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    NopagefoundComponent,
+    SidebarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, APP_ROUTES],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
